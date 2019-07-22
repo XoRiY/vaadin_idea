fa4ed612e660cf751eeb80577b59d86da16c548c
xoriy <kerdoudtahar@gmail.com>
2019-07-22 20:14:37 +0200

    add post method for a list of users and one user add get method to retrieve a list of users

--- src/main/java/com/vaadin/ktr/idea/domain/UsersController.java
+++ src/main/java/com/vaadin/ktr/idea/domain/UsersController.java
@@ -1,23 +1,45 @@
 package com.vaadin.ktr.idea.domain;
 
+import com.vaadin.ktr.idea.domain.model.User;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import com.vaadin.ktr.idea.domain.model.User;
 
 
 @RestController("/users")
 public class UsersController {
 
-
-  @PostMapping(consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+  @PostMapping(path = "/users/user",
+      consumes = MediaType.APPLICATION_JSON_UTF8_VALUE,
+      produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
   public ResponseEntity<String> postUser(@RequestBody User user) {
-
     return new ResponseEntity<>(HttpStatus.OK);
   }
 
+  @PostMapping(consumes = MediaType.APPLICATION_JSON_UTF8_VALUE,
+      produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+  public List<ResponseEntity<String>> postUsers(@RequestBody List<User> users) {
+    return Arrays.asList(new ResponseEntity<>(HttpStatus.OK),
+        new ResponseEntity<>(HttpStatus.OK), new ResponseEntity<>(HttpStatus.OK),
+        new ResponseEntity<>(HttpStatus.OK), new ResponseEntity<>(HttpStatus.OK));
+  }
 
+
+  @GetMapping(produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+  public List<User> getUsers() {
+    return Collections.singletonList(
+        new User()
+            .setEmail("test@email.com")
+            .setFirstName("firstTest")
+            .setLastName("lastTest")
+            .setId("TestId")
+    );
+  }
 }
