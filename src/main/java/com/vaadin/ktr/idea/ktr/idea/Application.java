b5dccbbd7893fcf54ae802ad1a965f2d3f8a4a49
xoriy <kerdoudtahar@gmail.com>
2019-07-22 18:48:25 +0200

    init project from https://start.spring.io/

---
+++ src/main/java/com/vaadin/ktr/idea/ktr/idea/Application.java
@@ -0,0 +1,13 @@
+package com.vaadin.ktr.idea.ktr.idea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
